52a180cb6d44819ca0c85d9a3c38c0b148173d26
wtempone <wtempone@MacBook-Pro-de-William.local>
2017-10-24 02:59:24 -0200

    diretiva de controle de acesso - com problemas no recarregamento

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { InputMaskDirective } from './input-mask/input-mask';
+import { ControleAcessoDirective } from './controle-acesso/controle-acesso';
 @NgModule({
 	declarations: [InputMaskDirective,
+    ControleAcessoDirective,
     ],
 	imports: [],
 	exports: [InputMaskDirective,
+    ControleAcessoDirective,
     ]
 })
 export class DirectivesModule {}
